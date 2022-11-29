c8bf91d2a5f967773695b7309c3cc4cc87e6e26f
Marius <ma89vik@gmail.com>
2022-11-29 11:29:00 +0800

    Let nginx handle base path

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ var indexRouter = require('./routes/index');
 var authRouter = require('./routes/auth');
 
 var app = express();
-var router = express.Router();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,7 +26,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 app.use(session({
   secret: 'keyboard cat',
   resave: false, // don't save session if unmodified
@@ -48,10 +47,8 @@ app.use(function(req, res, next) {
   next();
 });
 
-router.use('/', indexRouter);
-router.use('/', authRouter.router);
-
-app.use('/homestation', router);
+app.use('/', indexRouter);
+app.use('/', authRouter.router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
